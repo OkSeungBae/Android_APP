066911d952271f1b92f813b7ced324fc089a322d
osb1808 <osb1808@gmail.com>
2020-07-08 18:05:12 +0900

    [승배] 20200708 kakao map api 적용 및 test

--- 5.Android_Project_File/app/src/main/java/com/example/inforpet/MainActivity.java
+++ 5.Android_Project_File/app/src/main/java/com/example/inforpet/MainActivity.java
@@ -8,18 +8,28 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+import net.daum.mf.map.api.MapView;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup)findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
 
     }
 
