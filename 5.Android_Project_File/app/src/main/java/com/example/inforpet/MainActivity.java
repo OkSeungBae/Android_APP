d85ac24f5c7830f6466d4d0c71a462842af13d0a
osb1808 <osb1808@gmail.com>
2020-07-09 20:08:03 +0900

    [승배] 20200709 lib 파일 gradle 적용

--- 5.Android_Project_File/app/src/main/java/com/example/inforpet/MainActivity.java
+++ 5.Android_Project_File/app/src/main/java/com/example/inforpet/MainActivity.java
@@ -12,15 +12,25 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 
+import net.daum.android.map.coord.MapCoord;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
+
+import org.w3c.dom.Text;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
+import kr.hyosang.coordinate.CoordPoint;
+import kr.hyosang.coordinate.TransCoord;
+
 public class MainActivity extends AppCompatActivity {
 
     MapView mapView;
+    MapPOIItem marker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +41,19 @@ public class MainActivity extends AppCompatActivity {
         ViewGroup mapViewContainer = (ViewGroup)findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
 
+        CoordPoint pt = new CoordPoint(196470.449471829, 455970.888515918);
+        CoordPoint gwsPt = TransCoord.getTransCoord(pt, TransCoord.COORD_TYPE_TM, TransCoord.COORD_TYPE_WGS84);
+
+        Log.i("geoCode", "x :: " + gwsPt.x +" y :: " + gwsPt.y);
+
+        marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(gwsPt.y, gwsPt.x));      //      mapPointWithGeoCoord
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
+        mapView.addPOIItem(marker);
+
     }
-
-
 }
