f7db37a92e40a4974d20b100bdb4dc1781056782
YTae_Jun <dbsxowns34@gmail.com>
2020-07-14 16:26:31 +0900

    [태준] 2020 07 14  - 리뷰 레이아웃 작성

--- 5.Android_Project_File/app/src/main/java/com/example/inforpet/ReviewActivity.java
+++ 5.Android_Project_File/app/src/main/java/com/example/inforpet/ReviewActivity.java
@@ -2,13 +2,32 @@ package com.example.inforpet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ReviewActivity extends AppCompatActivity {
+
+    TextView name, address, call;
+    float rating_avg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        rating_avg = 4; //DB에 있는 rating 정보의 평균값을 가져옴
+
+        Button button = findViewById(R.id.button_review);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { //작성하기 버튼누르면 '리뷰작성' 액티비티 띄움
+                Intent intent = new Intent(getApplicationContext(), WriteReviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- 5.Android_Project_File/app/src/main/java/com/example/inforpet/WriteReviewActivity.java
+++ 5.Android_Project_File/app/src/main/java/com/example/inforpet/WriteReviewActivity.java
@@ -3,12 +3,32 @@ package com.example.inforpet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WriteReviewActivity extends AppCompatActivity {
+
+    TextView review, review_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
+
+        review_title = findViewById(R.id.reviewTitle);
+        review = findViewById(R.id.review);
+
+        Button button = findViewById(R.id.button_update);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //등록하기를 누르면 제목, 내용 DB에 저장하고 액티비티 종료
+                String title = review_title.getText().toString();
+                String context = review.getText().toString();
+
+                finish();
+            }
+        });
     }
 }
