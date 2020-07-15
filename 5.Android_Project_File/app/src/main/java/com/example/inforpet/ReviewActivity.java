de085328e2828f30a0502c65f5e643e4c1c99455
osb1808 <osb1808@gmail.com>
2020-07-15 17:43:12 +0900

    [승배] 2020 07 15 Review Activity 수정 및 네이게이션 바 색 수정

--- 5.Android_Project_File/app/src/main/java/com/example/inforpet/ReviewActivity.java
+++ 5.Android_Project_File/app/src/main/java/com/example/inforpet/ReviewActivity.java
@@ -1,27 +1,97 @@
 package com.example.inforpet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ReviewActivity extends AppCompatActivity {
+
+    ArrayList<Reviewer> reviewList;
+
+    //DB
+    DBManager dbManager;
+    SQLiteDatabase database;
 
     TextView name, address, call;
     float rating_avg;
     RatingBar ratingBar;
 
+    //받는 데이터
+    private String bplcNm, rdnWhlAddr, siteTel, opnSvcId, mgtNo;
+    private int opnSfTeamCode;
+
+    //리사이클 뷰 관련
+    RecyclerView recyclerView;
+    ReviewAdapter reviewAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        Intent intent = getIntent();
+        //PK
+        opnSvcId = intent.getStringExtra("opnSvcId");
+        opnSfTeamCode = intent.getIntExtra("opnSfTeamCode", 0);
+        mgtNo = intent.getStringExtra("mgtNo");
+        //PK
+        bplcNm = intent.getStringExtra("bplcNm");
+        rdnWhlAddr = intent.getStringExtra("rdnWhlAddr");
+        siteTel = intent.getStringExtra("siteTel");
+
+        //데이터 베이스 연결
+        createDatabase();
+
+        dbManager = new DBManager();
+
+        //review table만들기
+        dbManager.createReviewTable(database);
+
+        //review Table에 정보 다 받아오기
+        reviewList = dbManager.selectReviewTableAll(database, mgtNo);
+
+        name = findViewById(R.id.name_review);
+        address = findViewById(R.id.address_review);
+        call = findViewById(R.id.callnum_review);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        //recyclerView 의 layout 크기 고정
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        reviewAdapter = new ReviewAdapter();
+        reviewAdapter.setItems(reviewList);
+
+        /*
+        reviewAdapter.addItem(new Reviewer("kim", 4.5f, "2020-07-15", "동물병원 좋아용"));
+        reviewAdapter.addItem(new Reviewer("park", 4.5f, "2020-07-10", "동물병원 싫어용"));
+        */
+
+
+        recyclerView.setAdapter(reviewAdapter);
+
+        name.setText(bplcNm);
+        address.setText(rdnWhlAddr);
+        call.setText(siteTel);
+
 
         ratingBar = findViewById(R.id.ratingBar_reput);
         LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
@@ -34,10 +104,31 @@ public class ReviewActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) { //작성하기 버튼누르면 '리뷰작성' 액티비티 띄움
-                Intent intent = new Intent(getApplicationContext(), WriteReviewActivity.class);
-                startActivity(intent);
+                Intent writeReviewintent = new Intent(getApplicationContext(), WriteReviewActivity.class);
+
+                //PK
+                writeReviewintent.putExtra("opnSvcId",opnSvcId);
+                writeReviewintent.putExtra("opnSfTeamCode",opnSfTeamCode);
+                writeReviewintent.putExtra("mgtNo",mgtNo);
+                //PK
+
+                writeReviewintent.putExtra("bplcNm",bplcNm);
+                writeReviewintent.putExtra("rdnWhlAddr",rdnWhlAddr);
+                writeReviewintent.putExtra("siteTel",siteTel);
+                startActivity(writeReviewintent);
             }
         });
+    }
 
+    private void createDatabase()
+    {
+        try {
+            database = openOrCreateDatabase(MainActivity.TAG, MODE_PRIVATE, null);
+            Log.i(MainActivity.TAG, "ReivewActivity createDatabase() :: 데이터베이스 생성 또는 열기 성공" );
+        }catch (Exception e)
+        {
+            Log.i(MainActivity.TAG, "ReivewActivity createDatabase() :: 데이터베이스 생성 실패" );
+            e.printStackTrace();
+        }
     }
 }
